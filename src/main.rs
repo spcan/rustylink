1de48b560f531582f13c43d3988b9990baf5e9fc
spcan <32984627+spcan@users.noreply.github.com>
2019-05-09 17:46:29 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -66,22 +66,10 @@ fn main() {
 	println!("{}", link.read_core_regs().unwrap());
 	link.run();
 
-	let mut dbg = dbg::internal::Debugger::new();
-	dbg.load( std::path::PathBuf::from("/home/andres/micro_test/target/thumbv7em-none-eabihf/release/micro_test") );
-
-
 	link.halt();
 	debug!("Reading Vector table");
 
-	println!("{:?}", &dbg.elfs[0].vector.data[0..64] );
-
-	let data = link.read_mem(0x800_0000, 64).unwrap();
-
-	for i in 0..64 {
-		if data[i] != dbg.elfs[0].vector.data[i] {
-			warn!("Read data differs from ELF File");
-		}
-	}
+	println!("{:?}",  link.read_mem(0x800_0000, 64).unwrap() );
 
 	println!("New method:\n{:?}", link.read_mem32(0x800_0000, 60).unwrap() );
 	link.dump_data(64);
@@ -92,47 +80,4 @@ fn main() {
 		link.status();
 		std::thread::sleep(Duration::from_millis(20));
 	}
-}
-
-
-fn print_info(device: libusb::Device, desc: libusb::DeviceDescriptor, handle: libusb::DeviceHandle) {
-	let languages = match handle.read_languages(Duration::from_secs(1)) {
-		Ok(l) => l,
-		_ => panic!(),
-	};
-	println!("Found device {:x}:{:x}", desc.vendor_id(), desc.product_id());
-	println!("Manufacturer: {}", handle.read_manufacturer_string(languages[0], &desc, Duration::from_secs(1)).unwrap());
-	println!("Product: {}", handle.read_product_string(languages[0], &desc, Duration::from_secs(1)).unwrap());
-	let s = handle.read_serial_number_string(languages[0], &desc, Duration::from_secs(1)).unwrap().into_bytes();
-	print!("Serialnumber: ", );
-	for b in s {
-		print!("{:X}:", b);
-	}
-	print!("\n");
-	println!("At speed {} MBps", match device.speed() {
-		libusb::Speed::Low => 1.5,
-		libusb::Speed::Full => 12.0,
-		libusb::Speed::High => 480.0,
-		libusb::Speed::Super => 5000.0,
-		_ => 0.0,
-	});
-
-	let version = desc.device_version();
-
-	println!("STLink {:?}", version);
-	println!("STLink {:x}", common::version_to_raw( version ));
-
-	let cfg = match device.config_descriptor(0) {
-		Ok(c) => c,
-		_ => panic!(),
-	};
-
-	for interface in cfg.interfaces() {
-		for idesc in interface.descriptors() {
-			for endpoint in idesc.endpoint_descriptors() {
-				println!("{:?}", endpoint);
-			}
-		}
-	}
-
 }
